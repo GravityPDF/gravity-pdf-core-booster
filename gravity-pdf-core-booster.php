245a497fb67aed13d1bfa91caf7a895518186f4a
Jake Jackson <jake@gravitypdf.com>
2020-02-17 11:29:51 +1100

    Update copyright notice and simplify license code

--- gravity-pdf-core-booster.php
+++ gravity-pdf-core-booster.php
@@ -13,7 +13,7 @@
 
 /**
  * @package     Gravity PDF Core Booster
- * @copyright   Copyright (c) 2019, Blue Liquid Designs
+ * @copyright   Copyright (c) 2020, Blue Liquid Designs
  * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
  * @since       1.0
  */
@@ -22,26 +22,6 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-
-/*
-    This file is part of Gravity PDF Core Booster.
-
-    Copyright (c) 2019, Blue Liquid Designs
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
 
 define( 'GFPDF_CORE_BOOSTER_FILE', __FILE__ );
 define( 'GFPDF_CORE_BOOSTER_VERSION', '1.2.2' );
--- src/EnhancedOptions/Fields/AllCheckbox.php
+++ src/EnhancedOptions/Fields/AllCheckbox.php
@@ -9,7 +9,7 @@ use GFPDF\Helper\Helper_Abstract_Fields;
  * Gravity Forms Field
  *
  * @package     Gravity PDF
- * @copyright   Copyright (c) 2019, Blue Liquid Designs
+ * @copyright   Copyright (c) 2020, Blue Liquid Designs
  * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
  * @since       1.0
  */
@@ -18,26 +18,6 @@ use GFPDF\Helper\Helper_Abstract_Fields;
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-
-/*
-    This file is part of Gravity PDF Core Booster.
-
-    Copyright (c) 2019, Blue Liquid Designs
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
 
 /**
  * Controls the display and output of the Checkbox HTML
